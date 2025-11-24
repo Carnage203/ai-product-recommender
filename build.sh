c7f253405d2e52fb66b7c76d541c2e17844e92aa
Carnage203 <sohammandal49@gmail.com>
2025-11-24 15:01:13 +0530

    updated build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
 
-echo "Installing frontend dependencies..."
 cd frontend
 npm install
-
-echo "Building frontend..."
 npm run build
-
-echo "Installing Python dependencies..."
 cd ..
-pip install -r requirements.txt
 
-echo "Build complete!"
+pip install -r requirements.txt
